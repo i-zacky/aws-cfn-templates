1d65dc36e7291e46f491eda6a70db70c0098660a
i-zacky <aizaki.takayuki@gmail.com>
2023-12-05 00:23:06 +0900

    simple-s3-hostingのサンプル追加

--- simple-s3-hosting/README.md
+++ simple-s3-hosting/README.md
@@ -1,3 +1,6 @@
 # simple-s3-hosting
 
 ## System Architecture
+
+| ![simple-s3-hosting](https://github.com/i-zacky/aws-cfn-templates/assets/9291848/1c90fc75-b886-4ddc-93f9-b1543ac1142e) |
+| --- |
--- simple-s3-hosting/lib/s3-stack.ts
+++ simple-s3-hosting/lib/s3-stack.ts
@@ -1,6 +1,7 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3'
+import { ArnPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam'
 
 export class S3Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,16 +14,28 @@ export class S3Stack extends Stack {
     this.templateOptions.description = this.stackName
     this.templateOptions.transforms = ['AWS::Serverless-2016-10-31']
 
-    new Bucket(this, 'HostingBucket', {
+    const hostingBucket = new Bucket(this, 'HostingBucket', {
       bucketName: `${env}-${project}-source`,
       accessControl: BucketAccessControl.PRIVATE,
       blockPublicAccess: {
         blockPublicAcls: true,
-        blockPublicPolicy: true,
+        blockPublicPolicy: false,
         ignorePublicAcls: true,
-        restrictPublicBuckets: true,
+        restrictPublicBuckets: false,
       },
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: '404.html',
       removalPolicy: RemovalPolicy.DESTROY,
     })
+
+    const policy = new PolicyStatement({
+      sid: 'GetOnly',
+      effect: Effect.ALLOW,
+      principals: [new ArnPrincipal('*')],
+      actions: ['s3:GetObject'],
+      resources: [`${hostingBucket.bucketArn}/*`],
+    })
+
+    hostingBucket.addToResourcePolicy(policy)
   }
 }
